585ccf110076733c9c2539f815441a814e2399fb
LuisFarfan99 <lfarfan@koleyo.com>
2021-04-22 21:56:35 -0600

    Integración con mongodb terminada

--- routes/emprendimiento.js
+++ routes/emprendimiento.js
@@ -1,10 +1,9 @@
+const { ObjectId } = require('bson')
 const express = require('express')
 const router = express.Router()
 const database = require('../config/database')
 
 router.post('/nuevo', function (req, res, next) {
-    let emprendimientos = database.emprendimientos
-
     let nuevoEmprendimiento = {
         nombre: req.body.nombre,
         descripcion: req.body.descripcion,
@@ -15,55 +14,109 @@ router.post('/nuevo', function (req, res, next) {
         departamento: req.body.departamento,
         pais: req.body.pais,
         zona: req.body.zona,
-        categorias: [req.body.categorias]
+        categorias: req.body.categorias
     }
 
-    emprendimientos[req.body.id] = nuevoEmprendimiento
+    database.connect(function(err, client) {
+        if (err) next('Error al conectarse a la base de datos')
 
+        const db = client.db('test')
 
-    res.status(201).json({emprendimiento: emprendimientos[req.body.id]})
+        db.collection('Emprendimientos').insertOne(nuevoEmprendimiento)
+        .then(result => {
+            if (result.result.ok !== 1) {
+                client.close()
+                next('Error al insertar')
+            }
+            client.close()
+            res.status(201).json({emprendimiento: nuevoEmprendimiento})
+        })
+    })
 })
 
 router.get('/:id', function (req, res, next) {
-    const emprendimientoEncontrado = database.emprendimientos[req.params.id]
+    database.connect(function(err, client) {
+        if (err) next('Error al conectarse a la base de datos')
 
-    if (!emprendimientoEncontrado) next('No se encontro el negocio')
+        const db = client.db('test')
+        const query = {
+            _id: ObjectId(req.params.id)
+        }
 
-    res.status(200).json({emprendimiento: emprendimientoEncontrado})
+        db.collection('Emprendimientos').findOne(query).then(result => {
+            if (!result) {
+                client.close()
+                res.status(500).json({message: 'No se encontro el negocio'})
+            }
+            console.log(result)
+            client.close()
+            res.status(201).json({emprendimiento: result})
+        })
+    })
 })
 
 router.put('/:id', function (req, res, next) {
-    let emprendimientoEncontrado = database.emprendimientos[req.params.id]
+    database.connect(function(err, client) {
+        if (err) next('Error al conectarse a la base de datos')
 
-    if (!emprendimientoEncontrado) next('No se encontro el negocio')
+        const db = client.db('test')
 
-    emprendimientoEncontrado = {
-        nombre: req.body.nombre,
-        descripcion: req.body.descripcion,
-        telefono: req.body.telefono,
-        email: req.body.email,
-        webPage: req.body.webPage,
-        municipio: req.body.municipio,
-        departamento: req.body.departamento,
-        pais: req.body.pais,
-        zona: req.body.zona,
-        categorias: [req.body.categorias]
-    }
-
-    database.emprendimientos[req.body.id] = emprendimientoEncontrado
-
-
-    res.status(204).json({emprendimiento: emprendimientoEncontrado})
+        db.collection('Emprendimientos').findOneAndUpdate(
+            {
+                _id: ObjectId(req.params.id)
+            },
+            {
+                $set: {
+                    nombre: req.body.nombre,
+                    descripcion: req.body.descripcion,
+                    telefono: req.body.telefono,
+                    email: req.body.email,
+                    webPage: req.body.webPage,
+                    municipio: req.body.municipio,
+                    departamento: req.body.departamento,
+                    pais: req.body.pais,
+                    zona: req.body.zona,
+                    categorias: req.body.categorias
+                }
+            },
+            {
+                returnOriginal: false
+            }
+        ).then(result => {
+            if (!result.value) {
+                client.close()
+                res.status(500).json({message: 'No se encontro el negocio'})
+            }
+            console.log(result)
+            client.close()
+            res.status(202).json({emprendimiento: result.value})
+        })
+    })
 })
 
 router.delete('/:id', function (req, res, next) {
-    const emprendimientoEncontrado = database.emprendimientos[req.params.id]
+    database.connect(function(err, client) {
+        if (err) next('Error al conectarse a la base de datos')
 
-    if (!emprendimientoEncontrado) next('No se encontro el negocio')
+        const db = client.db('test')
+        const query = {
+            _id: ObjectId(req.params.id)
+        }
 
-    delete database.emprendimientos[req.params.id]
-
-    res.status(204).json({usario: database.emprendimientos[req.params.id]})
+        db.collection('Emprendimientos').findOneAndDelete(
+            {
+                _id: ObjectId(req.params.id)
+            }
+        ).then(result => {
+            console.log(result)
+            if (!result.ok) {
+                client.close()
+                res.status(500).json({message: 'Error'})
+            }
+            client.close()
+            res.status(204).json({emprendimiento: result})
+        })
+    })
 })
 
 module.exports = router
--- routes/usuario.js
+++ routes/usuario.js
@@ -37,6 +37,20 @@ router.post('/nuevo', function(req, res, next) {
     })
 })
 
+router.get('/', function(req, res, next) {
+    database.connect(function(err, client) {
+        if (err) next('Error al conectarse a la base de datos')
+
+        const db = client.db('test')
+
+        db.collection('Usuarios').find().toArray((err, result) => {
+            console.log(result)
+            client.close()
+            res.status(201).json({usuarios: result})
+        })
+    })
+})
+
 router.get('/:id', function(req, res, next) {
     database.connect(function(err, client) {
         if (err) next('Error al conectarse a la base de datos')
@@ -59,33 +73,67 @@ router.get('/:id', function(req, res, next) {
 })
 
 router.put('/:id', function(req, res, next) {
-    let usuarioEncontrado = database.usuarios[req.params.id]
+    database.connect(function(err, client) {
+        if (err) next('Error al conectarse a la base de datos')
 
-    if (!usuarioEncontrado) next('No se encontro el usuario')
+        const db = client.db('test')
+        const query = {
+            _id: ObjectId(req.params.id)
+        }
 
-    usuarioEncontrado = {
-        nombre: req.body.nombre,
-        apellido: req.body.apellido,
-        telefono: req.body.telefono,
-        fechaNacimiento: req.body.fechaNacimiento,
-        email: req.body.email,
-        username: req.body.username,
-        password: req.body.password
-    }
-
-    database.usuarios[req.params.id] = usuarioEncontrado
-
-    res.status(204).json({usuario: database.usuarios[req.params.id]})
+        db.collection('Usuarios').findOneAndUpdate(
+            {
+                _id: ObjectId(req.params.id)
+            },
+            {
+                $set: {
+                    nombre: req.body.nombre,
+                    apellido: req.body.apellido,
+                    telefono: req.body.telefono,
+                    fechaNacimiento: req.body.fechaNacimiento,
+                    email: req.body.email,
+                    username: req.body.username,
+                    password: req.body.password
+                }
+            },
+            {
+                returnOriginal: false
+            }
+        ).then(result => {
+            if (!result) {
+                client.close()
+                res.status(500).json({message: 'No se encontro el usuario'})
+            }
+            console.log(result.value)
+            client.close()
+            res.status(202).json({usuario: result.value})
+        })
+    })
 })
 
 router.delete('/:id', function(req, res, next) {
-    const usuarioEncontrado = database.usuarios[req.params.id]
+    database.connect(function(err, client) {
+        if (err) next('Error al conectarse a la base de datos')
 
-    if (!usuarioEncontrado) next('No se encontro el usuario')
+        const db = client.db('test')
+        const query = {
+            _id: ObjectId(req.params.id)
+        }
 
-    delete database.usuarios[req.params.id]
-
-    res.status(204).json({usario: database.usuarios[req.params.id]})
+        db.collection('Usuarios').findOneAndDelete(
+            {
+                _id: ObjectId(req.params.id)
+            }
+        ).then(result => {
+            console.log(result)
+            if (!result.ok) {
+                client.close()
+                res.status(500).json({message: 'Error'})
+            }
+            client.close()
+            res.status(204).json({usuario: result})
+        })
+    })
 })
 
 module.exports = router
